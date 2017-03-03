8539c8f7f7e64ab82063eb4a4f072d5a1834f148
Shahul <shahul493@gmail.com>
2017-03-03 10:23:39 +0530

    view-edit pages

---
+++ idlydabba/view-edit-page/js/common.js
@@ -0,0 +1,121 @@
+(function($) {
+ "use strict"
+	// scroll up
+	jQuery(window).on("scroll", function(){
+		if (jQuery(this).scrollTop() > 1) {
+			jQuery('.scrollup').css({bottom:"25px"});
+		} else {
+			jQuery('.scrollup').css({bottom:"-100px"});
+		}
+	});
+	jQuery('.scrollup').on("click", function() {
+		jQuery('html, body').animate({scrollTop: '0px'}, 800);
+		return false;
+	});
+	
+	// Page Preloader
+	$(window).on("load", function() {
+		$(".loader").delay(300).fadeOut();
+		$(".animationload").delay(600).fadeOut("slow");
+	});    
+    $('.accordion').find('.accordion-toggle').click(function() {
+        $(this).next().slideToggle('600');
+        $(".accordion-content").not($(this).next()).slideUp('600');
+	});
+	$('.accordion-toggle').on('click', function() {
+		$(this).toggleClass('active').siblings().removeClass('active');
+	});
+	var d = new Date();
+    document.getElementById("date").innerHTML = d.toDateString();    
+
+})(jQuery);
+
+
+google.charts.load('current', { 'packages': ['corechart'] });
+
+
+function datepickertext()
+{
+    var dte = new Date();
+    var defautlDate = dte.toDateString("dd/mm/yyyy");
+    //$("#car-time").html(defautlDate);
+    //document.getElementById('time').html = defautlDate;
+    $("#from").val(defautlDate);
+    $("#to").val(defautlDate);
+    $("#from").datepicker({
+        defaultDate: defautlDate,
+        changeMonth: true,
+        numberOfMonths: 3,
+        format: 'dd/mm/yyyy',
+
+        todayHighlight: true,
+        onClose: function (selectedDate) {
+            $("#to").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#to").datepicker({
+        defaultDate: defautlDate,
+        changeMonth: true,
+
+        todayHighlight: true,
+        format: 'dd/mm/yyyy',
+        numberOfMonths: 3,
+        onClose: function (selectedDate) {
+            $("#from").datepicker("option", "maxDate", selectedDate);
+        }
+    });
+
+    $("#fromattendance").datepicker({
+        defaultDate: defautlDate,
+        changeMonth: true,
+        numberOfMonths: 3,
+        format: 'dd/mm/yyyy',
+
+        todayHighlight: true,
+        onClose: function (selectedDate) {
+            $("#toattendance").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#toattendance").datepicker({
+        defaultDate: defautlDate,
+        changeMonth: true,
+
+        todayHighlight: true,
+        format: 'dd/mm/yyyy',
+        numberOfMonths: 3,
+        onClose: function (selectedDate) {
+            $("#fromattendance").datepicker("option", "maxDate", selectedDate);
+        }
+    });
+
+    $("#neworderdate").datepicker({
+        defaultDate: defautlDate,
+        changeMonth: true,
+        numberOfMonths: 3,
+        format: 'dd/mm/yyyy',
+
+        todayHighlight: true,
+        onClose: function (selectedDate) {
+            //$("#to").datepicker("option", "minDate", selectedDate);
+        }
+    });
+   
+}
+
+
+$(function(){
+
+    var iconOpen = 'fa fa-minus-circle';
+    
+    var iconClose = 'fa fa-plus-circle';
+
+    $(document).on('show.bs.collapse hide.bs.collapse', '#faq-accordion', function (e) {
+        var $target = $(e.target)
+        $target.siblings('.accordion-heading')
+        .find('em').toggleClass(iconOpen + ' ' + iconClose);
+        if (e.type == 'show')
+            $target.prev('.accordion-heading').find('.accordion-toggle').addClass('active');
+        if (e.type == 'hide')
+            $(this).find('.accordion-toggle').not($target).removeClass('active');
+    });
+});
---
+++ idlydabba/view-edit-page/js/tabledata.js
@@ -0,0 +1,74 @@
+﻿
+var dataSet = [
+      ["Student", "Student", "1a1", "Primary", "2nd", "1a1", "Primary", "Primary", "Primadssssssssssssry", "Primary", "Primary", "Primary", "Primary", "Primary", "Primary"],
+      ["Student", "Student", "1a1", "Primary", "2nd", "1a1", "", "", "", "", "", "Primary", "", "Primary", ""],
+      ["Student", "Student", "1a1", "Primary", "2nd", "1a1", "", "", "", "", "", "Primary", "", "Primary", ""],
+
+];
+
+var dataSetma = [
+["Subramani", "4th-C", "17M601", "B-M1", "No"],
+["Vimal", "5th-D", "17M201", "B-M2", "Yes"],
+["Adhi", "6th-E", "17M251", "C-M1", "No"],
+
+];
+
+var datamealnew = [
+["12/01/17", "Breakfast", "M1", "Veg", "20"], ["11/01/17", "lunch", "M2", "Veg", "80"], ["14/01/17", "dinner", "M3", "Veg", "30"], ["12/01/17", "Dinner", "M4", "Veg", "50"], ["12/01/17", "Breakfast", "M1", "Veg", "20"]
+
+
+
+];
+
+
+function bindtable() {
+    $('#tbl-category').DataTable({
+        "sDom": 'rt',
+        data: dataSet,
+        resize: false,
+        "responsive": true,
+        columnDefs: [
+
+           { responsivePriority: 1, targets: 0 },
+        ]
+    });
+    $('#tbl-meal-new').DataTable({
+        "sDom": 'rt',
+        data: datamealnew,
+        resize: false,
+        "responsive": true,
+        columnDefs: [
+
+           { responsivePriority: 1, targets: 0 },
+        ]
+    });
+
+    $('#tbl-meal-attendance').DataTable({
+        "sDom": 'rt',
+        data: dataSetma,
+        "bAutoWidth": false,
+        "scrollX": true,
+        "scrollY": "200px",
+        
+        "paging": false,
+        "bScrollCollapse": true,
+        "columnDefs": [ {
+            "targets": -1,
+            "data": null,
+            "render": function (data, type, row) {
+                // return data + ' (' + row[3] + ')';
+                if (row[4] === "No") {
+                    return '<label class="checkbox-inline"><input class="chkbx" type="checkbox" data-toggle="toggle"> Absent</label>';
+                }
+                else
+                {
+                    return '<label class="checkbox-inline"><input class="chkbx" type="checkbox" checked data-toggle="toggle"> Present</label>';
+                }
+            },
+            
+        }],
+        "fnDrawCallback": function () {
+            $('.chkbx').bootstrapToggle();
+        },
+    });
+}
